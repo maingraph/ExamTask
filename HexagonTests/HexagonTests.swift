2f187624c02df3f2c33b4c63f68800797ce1d8c5
imjustchilling <andreyalekswork@gmail.com>
2025-06-17 10:18:19 +0300

    Initial Commit

---
+++ HexagonTests/HexagonTests.swift
@@ -0,0 +1,17 @@
+//
+//  HexagonTests.swift
+//  HexagonTests
+//
+//  Created by abc on 17.06.2025.
+//
+
+import Testing
+@testable import Hexagon
+
+struct HexagonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
